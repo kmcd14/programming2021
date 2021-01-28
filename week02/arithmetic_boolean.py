53ce040496c9f3dc084121fbf34d6eb31c811214
Katie Mc D <katieisanimdom@gmail.com>
2021-01-28 11:37:46 +0000

    week 2

---
+++ week02/arithmetic_boolean.py
@@ -0,0 +1,6 @@
+# arithmetic_boolean.py
+# Author Katie Mc Donald
+
+print(21 - 4)
+
+print(2 == 3)
---
+++ week02/name_and_age.py
@@ -0,0 +1,8 @@
+# name_and_age.py
+# ask for your name and age
+# Author: Katie Mc Donald
+
+name = input('What is your name? ')
+age = input('What is your age? ')
+
+print('Hello {}.\t Your age is {}'.format(name, age))
